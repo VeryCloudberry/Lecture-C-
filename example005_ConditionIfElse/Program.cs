ca11d94c20ea46bcab6e6c569bf9f2d06b696f1d
Alina <smila_@mail.ru>
2023-04-26 00:23:06 +0300

    add Example 005 ConditionIfElse

---
+++ example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maxim")
+{
+    Console.WriteLine("Waw, it's Maxim!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}

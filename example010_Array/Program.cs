3d1c046ee250a471aa01635c204d0da023cc71eb
Alina <smila_@mail.ru>
2023-05-02 08:26:15 +0300

    add Example 010 Array

---
+++ example010_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {1, 12, 34, 81, 4, 51, 16, 17, 81};
+int n = array.Length;
+int find = 81;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1
+    index++;
+}
